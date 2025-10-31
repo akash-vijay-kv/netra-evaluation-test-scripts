8217b822ed41b917a8c705749ac3538e193d52e8
akash-vijay-kv <akash@keyvalue.systems>
2025-10-31 22:19:39 +0530

    refactor: updated evaluation pipeline

--- src/evaluation_pipeline.py
+++ src/evaluation_pipeline.py
@@ -7,31 +7,27 @@ load_dotenv()
 async def main():
     
     # V1 Agent Test Run
-    # from library_assistant_v1 import get_library_agent_response
-
-    # dataset = Netra.evaluation.get_dataset(dataset_id="17265fe3-ef5b-43ec-a358-555a189fc0a4")
-
-    # result = Netra.evaluation.run_test_suite(
-    #     name="Library Assistant v1",
-    #     data=dataset,
-    #     task=get_library_agent_response,
-    # )
-
-    # print(result)
-
-    # V2 Agent Test Run
-    from library_assistant_v2 import get_library_agent_response
+    from library_assistant_v1 import get_library_agent_response
 
     dataset = Netra.evaluation.get_dataset(dataset_id="17265fe3-ef5b-43ec-a358-555a189fc0a4")
 
     result = Netra.evaluation.run_test_suite(
-        name="Library Assistant v2",
+        name="Library Assistant v1",
         data=dataset,
         task=get_library_agent_response,
     )
 
-    print(result)
 
+    # V2 Agent Test Run
+    # from library_assistant_v2 import get_library_agent_response
+
+    # dataset = Netra.evaluation.get_dataset(dataset_id="17265fe3-ef5b-43ec-a358-555a189fc0a4")
+
+    # result = Netra.evaluation.run_test_suite(
+    #     name="Library Assistant v2",
+    #     data=dataset,
+    #     task=get_library_agent_response,
+    # )
 
 if __name__ == "__main__":
     asyncio.run(main())
